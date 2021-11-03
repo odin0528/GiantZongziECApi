4df4f2278a2a499a1187db0d088f2c96efa81ff9
odin0528 <odin0528@hotmail.com>
2021-11-03 20:57:20 +0800

    remove withdebug

--- controllers/backend/ecpay.go
+++ controllers/backend/ecpay.go
@@ -44,7 +44,6 @@ func EcpayPaymentFinish(c *gin.Context) {
 	delete(params, "CheckMacValue")
 	client := ecpay.NewStageClient(
 		ecpay.WithReturnURL(fmt.Sprintf("%s%s", os.Getenv("API_URL"), os.Getenv("ECPAY_PAYMENT_FINISH_URL"))),
-		ecpay.WithDebug,
 	)
 	mac := client.GenerateCheckMacValue(params)
 	if mac != senderMac {
