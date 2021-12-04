429d330b2fe40adf028efccc8e4c8ff17490e8c8
Odin <odin9130@gmail.com>
2021-12-05 03:11:21 +0800

    打錯字改欄位長度

--- sql.sql
+++ sql.sql
@@ -39,11 +39,12 @@ CHANGE COLUMN `group` `group_no` tinyint(255) NULL DEFAULT NULL AFTER `product_i
 ALTER TABLE `orders` 
 ADD COLUMN `paid_at` int(11) NULL COMMENT '付款時間' AFTER `status`,
 ADD COLUMN `delivered_at` int(11) NULL COMMENT '到貨時間' AFTER `paid_at`,
-ADD COLUMN `picker_id` int(11) NULL DEFAULT 0 COMMENT '撿貨者id' AFTER `member_id`
+ADD COLUMN `picker_id` int(11) NULL DEFAULT 0 COMMENT '撿貨者id' AFTER `member_id`,
+MODIFY COLUMN `logistics_status` tinyint(4) UNSIGNED NULL DEFAULT 0 COMMENT '物流狀態' AFTER `shipment_no`,
 MODIFY COLUMN `created_at` int(11) NULL DEFAULT NULL AFTER `status`;
 
 UPDATE orders SET `status` = 51 WHERE `status` = 31;
 UPDATE orders SET `status` = 99 WHERE `status` = -99;
-UPDATE orders SET `logistics_status` = 110 WHERE `logsitics_status` = 3;
-UPDATE orders SET `logistics_status` = 120 WHERE `logsitics_status` = 4;
-UPDATE orders SET `logistics_status` = 199 WHERE `logsitics_status` = 5;
+UPDATE orders SET `logistics_status` = 110 WHERE `logistics_status` = 3;
+UPDATE orders SET `logistics_status` = 120 WHERE `logistics_status` = 4;
+UPDATE orders SET `logistics_status` = 199 WHERE `logistics_status` = 5;
